536b54ddb71d947606298fc675b662a87c1e62ae
Michał Biernacki <michalbiernacki95@o2.pl>
2022-02-04 20:58:01 +0100

    commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+bezJacka = document.getElementById("bezJacka");
+z = document.getElementById("z");
+bez = document.getElementById("bez");
+
+function changeBackground()
+{
+   bezJacka.style.backgroundImage = "url('2.png')";
+   bez.style.display = "none";
+   z.style.display = "block";
+}
+
+function changeBackground2()
+{
+   bezJacka.style.backgroundImage = "url('Bez Jacka.png')";
+   z.style.display = "none";
+   bez.style.display = "block";
+}
